c298e766f5a6f012f78fce1932fdcf2a92144614
Serhii S. Stasiuk <stas@onlineua.net>
2022-06-03 10:48:21 +0300

    Added templates

--- src/armypapers/personnel/urls.py
+++ src/armypapers/personnel/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+from personnel.views import OrderDetailView, OrderListView, WarriorDetailView, WarriorListView
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('<slug:number>/', OrderDetailView.as_view(), name='order-detail'),
+    #path('order<int:number>/', OrderDetailView.as_view(), name='order-detail'),
+    path('orders/', OrderListView.as_view(), name='order-list'),
+    path('order/<int:pk>', OrderDetailView.as_view(), name='order-detail'),
+    path('warriors/', WarriorListView.as_view(), name='warrior-list'),
+    path('warrior/<int:pk>', WarriorDetailView.as_view(), name='warrior-detail'),
 ]
--- src/armypapers/personnel/views.py
+++ src/armypapers/personnel/views.py
@@ -2,6 +2,43 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
+
+from personnel.models import Order, Warrior
+
+class OrderDetailView(DetailView):
+
+    model = Order
+
+#    def get_context_data(self, **kwargs):
+#        context = super().get_context_data(**kwargs)
+#        return context
+
+class OrderListView(ListView):
+
+    model = Order
+    paginate_by = 10
+
+#    def get_context_data(self, **kwargs):
+#        context = super().get_context_data(**kwargs)
+#        return context
+
+class WarriorDetailView(DetailView):
+
+    model = Warrior
+
+#    def get_context_data(self, **kwargs):
+#        context = super().get_context_data(**kwargs)
+#        return context
+
+class WarriorListView(ListView):
+
+    model = Warrior
+
+#    def get_context_data(self, **kwargs):
+#        context = super().get_context_data(**kwargs)
+#        return context
